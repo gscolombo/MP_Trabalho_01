4eaa934837da2a4f1cf58df9dc79648d264bf907
Gabriel de Souza Colombo <222014062@aluno.unb.br>
2023-09-20 23:19:47 -0300

    Desenvolvimento do programa principal

--- velha.cpp
+++ velha.cpp
@@ -5,7 +5,26 @@
 int main()
 {
     vector<vector<int>> game = parseHash();
+    int res = validateTicTacToe(game);
+    
+    switch (res)
+    {
+    case 2:
+        cout << "Circles wins!";
+        break;
+    case 1:
+        cout << "Crosses wins!";
+        break;
+    case 0:
+        cout << "Game tied!";
+        break;
+    case -1:
+        cout << "No winners yet";
+        break;
+    case -2:
+        cout << "Game is invalid!";
+        break;
+    }
 
-     
-    return 0;
+    return res;
 }
